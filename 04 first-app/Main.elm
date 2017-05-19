f6bfb3d08636bc9e531451df50c872db73a2e0c1
Charlie <charlie@wizeline.com>
2017-05-18 23:28:54 -0500

    better calories

--- 04 first-app/Main.elm
+++ 04 first-app/Main.elm
@@ -3,18 +3,25 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import String
 
 
 -- Model
 
 
 type alias Model =
-    Int
+    { calories : Int
+    , input : Int
+    , err : Maybe String
+    }
 
 
 initModel : Model
 initModel =
-    0
+    { calories = 0
+    , input = 0
+    , err = Nothing
+    }
 
 
 
@@ -24,16 +31,42 @@ initModel =
 type Msg
     = AddCalorie
     | Clear
+    | ChangeInput String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddCalorie ->
-            model + 1
+            { model
+                | calories = model.calories + model.input
+                , input = 0
+            }
+
+        ChangeInput val ->
+            case String.toInt val of
+                Ok input ->
+                    { model
+                        | input = input
+                        , err = Nothing
+                    }
+
+                Err err ->
+                    { model
+                        | input = 0
+                        , err = Just err
+                    }
 
         Clear ->
             initModel
+
+
+validateInput : Int -> String
+validateInput input =
+    if input == 0 then
+        ""
+    else
+        toString input
 
 
 
@@ -44,7 +77,14 @@ view : Model -> Html Msg
 view model =
     div []
         [ h3 []
-            [ text ("Total calories: " ++ toString model) ]
+            [ text ("Total calories: " ++ toString model.calories) ]
+        , input
+            [ placeholder "Type something"
+            , onInput ChangeInput
+            , value (validateInput model.input)
+            ]
+            []
+        , div [] [ text (Maybe.withDefault "" model.err) ]
         , button
             [ type_ "button"
             , onClick AddCalorie
